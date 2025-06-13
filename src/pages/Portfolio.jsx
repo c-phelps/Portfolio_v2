6d7efa838762b97c0f993a2a71f4d2546453a8c5
Chris Phelps <c.j.phelps89@gmail.com>
2025-06-12 21:07:36 -0400

    NavBar removed

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,7 +3,6 @@ import AboutMe from "../components/about-me/AboutMe";
 import CarouselWithInfo from "../components/projects-display/CarouselWithInfo";
 import ContactMe from "../components/contact-me/ContactMe";
 import Footer from "../components/footer/Footer";
-import Navbar from "../components/navbar/Navbar";
 
 export default function Portfolio() {
   return (
